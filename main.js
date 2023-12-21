fc41954227e3d5144f36d9eb15e2f14ecf59c980
Hellk1per <154437865+Hellk1per@users.noreply.github.com>
2023-12-21 14:38:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleImage2() { 
+    var img = document.getElementById("lab2"); 
+    if (img.style.display === "none") { 
+      img.style.display = "block"; 
+    } else { 
+      img.style.display = "none"; 
+    } 
+}
